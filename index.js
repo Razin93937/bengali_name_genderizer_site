c002a41f54e286a0e2fd92d62bc05f75a3d3b160
Razin <ssrazin@hotmail.com>
2020-05-09 19:41:52 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+var name_f;
+
+$(document).ready(function () {
+    name_f = document.getElementById("name_input");
+    $("#alert").hide();
+})
+
+$(document).on('click', '#submit', function (evt) {
+    evt.preventDefault();
+    var name = name_f.value;
+    if (name == "") {
+        $("#alert").show();
+    }
+    else {
+        var requestURL = "http://razin93937.pythonanywhere.com/guess/";
+        $.post(requestURL, { 'f_name': name }, function (data) {
+            $("#alert").hide();
+            $("#rows").empty();
+            for (let i = 0; i < Object.keys(data['children']).length; i++) {
+                child = data['children'][i];
+                $("#rows").append("<tr id='row" + i + "'></tr>");
+
+                $("#row" + i).append("<td>" + child['word'] + "</td>");
+                $("#row" + i).append("<td>" + child['gender'] + "</td>");
+                $("#row" + i).append("<td>" + child['confidence'] + "</td>");
+            }
+            if (data['verdict'] != "") {
+                $("#verdict").text(name + " is probably a " + data['verdict'] + " name");
+            } else {
+                $("#verdict").text("Cannot conclude name of " + name);
+            }
+
+        }, "json");
+    }
+});
