596581569d88f09606e1faa45b64997b4f949dff
Razin <ssrazin@hotmail.com>
2020-05-12 14:12:56 +0600

    removed verdict box, incorporated new ww model

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-var name_f;
 var name;
 var requestURL = "https://razin93937.pythonanywhere.com/";
 // var requestURL = "http://127.0.0.1:5000/";
 
 $(document).ready(function () {
-    name_f = document.getElementById("name_input");
     update_selection();
-
+    if (name != "") $("#name_input").val(name);
     $("#model_selection").change(function () {
         update_selection();
     });
@@ -14,7 +12,7 @@ $(document).ready(function () {
 
 $(document).on('click', '#submit', function (evt) {
     evt.preventDefault(); // prevent it from submitting default
-    name = name_f.value;
+    name = $("#name_input").val();
 
     //check if name is entered, if not show alert
 
@@ -39,9 +37,9 @@ function predict_full_name() {
         $("#alert").hide();
         $("#rows").empty();
         $("#rows").append("<tr id='row1'></tr>");
+        $("#row1").append("<td>" + name + "</td>");
         $("#row1").append("<td>" + data['verdict'] + "</td>");
-        $("#row1").append("<td>" + (data['confidence'] - 0.5) * 2 + "</td>");
-        $("#verdict").text("I am " + ((data['confidence'] * 100 - 50) * 2).toFixed(2) + "% sure \"" + name + "\" is a " + data['verdict'] + " name");
+        $("#row1").append("<td>" + ((data['confidence'] * 100 - 50) * 2).toFixed(2) + "%</td>");
     }, "json");
 }
 
@@ -58,35 +56,18 @@ function predict_word_wise() {
 
             $("#row" + i).append("<td>" + child['word'] + "</td>");
             $("#row" + i).append("<td>" + child['gender'] + "</td>");
-            $("#row" + i).append("<td>" + child['confidence'] + "</td>");
+            $("#row" + i).append("<td>" + (child['confidence']).toFixed(2) + "%</td>");
         }
-
-        // if name has only 1 word, don't show final verdict
-
-        if (Object.keys(data['children']).length > 1) {
-            if (data['verdict'] != "") {
-                $("#verdict").text("\"" + name + "\" is probably a " + data['verdict'] + " name");
-            } else {
-                $("#verdict").text("Cannot conclude gender of " + name);
-            }
-        } else {
-            $("#verdict").text("Enter full name for overall verdict");
-        }
-
     }, "json");
 }
 
 function update_selection() {
     if ($("#model_selection").find(":selected").val() == "1") {
         // when full_name is selected
-        $("#col1").hide(); // hide name column
-        //$("#verdict_box").hide(); // hide final verdict box
         $("#rows").empty(); // empty the table
         if (name != "") predict_full_name();
     } else {
         // when word-wise is selected
-        $("#col1").show(); // show name column
-        //$("#verdict_box").show(); // show final verdict box
         $("#rows").empty(); //empty the table
         if (name != "") predict_word_wise();
     }
